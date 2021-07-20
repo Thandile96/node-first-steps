a6b5451a7ff78828a478c69f01e02665b9113b42
Athandile_Nkcaza <nkcazaathandile@gmail.com>
2021-07-20 16:33:28 +0200

    initial commit

--- greet_xola.js
+++ greet_xola.js
@@ -4,3 +4,37 @@ const greet = require('./greet');
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
 console.log(styledMessage)
+
+var figlet = require('figlet');
+ 
+figlet('Hello , Xola', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
+figlet.text('Hello , Xola', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
+
+console.log(figlet.textSync('Hello, Xola', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}));
